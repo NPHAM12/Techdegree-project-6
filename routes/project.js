facf8fa69477086ad5d897cc491b6ed932ffcce9
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-06-01 00:55:01 -0700

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -8,8 +8,8 @@ const listOfProjects = require('../data.json').projects; //array of objects
 // Route to Project page
 router.get('/projects/:id', (req, res) => {
   //Route path: /todos/:id
-  //Request URL: http://localhost:xxxx/todos/36
-  //req.params : {'id': '36'}
+  //Request URL: http://localhost:xxxx/todos/01
+  //req.params : {'id': '01'}
   const id = req.params.id;
   const proj = listOfProjects[id];
   // if user enter a number in URL is larger than amount of projects
@@ -20,7 +20,9 @@ router.get('/projects/:id', (req, res) => {
   else {
     res.render('project', {proj}); //render projects on project page
     //'project' here is 'project.pug'
-    //project: listOfProjects[id] -> project is an object variable -> project.pug
+    //proj is an object variable is passed to template project.pug
   }
 });
+
+//export router
 module.exports = router;
